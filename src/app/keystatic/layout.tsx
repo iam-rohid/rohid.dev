f24d7b6a7b7b7674cb18d30bba440161ad404a64
Rohid <rohidul209@gmail.com>
2023-07-07 09:25:48 +0600

    keystatic title

--- src/app/keystatic/layout.tsx
+++ src/app/keystatic/layout.tsx
@@ -1,5 +1,9 @@
+import { Metadata } from "next";
 import KeystaticApp from "./keystatic";
 
+export const metadata: Metadata = {
+  title: "Keystatic",
+};
 export default function RootLayout() {
   return (
     <html>
